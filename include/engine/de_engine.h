8f7e3e2a59765c9b40ac464f754f3392c5df0a22
Harry Chou <harryyc.chou@gmail.com>
2024-02-22 00:03:25 -0800

    implement create pipeline and create pipeline layout functions

--- include/engine/de_engine.h
+++ include/engine/de_engine.h
@@ -3,9 +3,13 @@
 #include "engine/de_device.h"
 #include "engine/de_swap_chain.h"
 
+#include <vector>
 #include <memory>
+#include <stdexcept>
 
+using std::vector;
 using std::unique_ptr;
+using std::runtime_error;
 
 namespace de {
 class Engine{
@@ -17,21 +21,23 @@ class Engine{
   // constructor
   Engine();
   ~Engine();
+  Engine(const Engine&) = delete;
+  Engine& operator=(const Engine&) = delete;
 
   // function
   void run();
 
  private:
+  void CreatePipeline();
+  void CreatePipelineLayout();
+  void CreateCommandBuffers();
+  void DrawFrame();
+
   DeWindow de_window{WIDTH, HEIGHT, "Engine"};
   DeDevice de_device{de_window};
   DeSwapChain de_swap_chain{de_device, de_window.get_extent()};
   unique_ptr<DePipeline> de_pipeline;
-  /*
-  DePipeline de_pipeline{
-      de_device,
-      "../shaders/build/vert.spv",
-      "../shaders/build/frag.spv",
-      DePipeline::DefaultPipelineConfigInfo(WIDTH, HEIGHT)};
-  */
+  VkPipelineLayout pipeline_layout;
+  vector<VkCommandBuffer> command_buffers;
 };
 }
--- src/engine/de_engine.cc
+++ src/engine/de_engine.cc
@@ -1,11 +1,53 @@
 #include "engine/de_engine.h"
 
 namespace de {
-Engine::Engine() {}
-Engine::~Engine() {}
+Engine::Engine() {
+  CreatePipelineLayout();
+  CreatePipeline();
+  CreateCommandBuffers();
+}
+
+Engine::~Engine() {
+  vkDestroyPipelineLayout(de_device.device(), pipeline_layout, nullptr);
+}
+
 void Engine::run() {
   while (!de_window.ShouldClose()) {
     glfwPollEvents();
   }
 }
+
+void Engine::CreatePipeline() {
+  auto pipelineConfig =
+      DePipeline::DefaultPipelineConfigInfo(de_swap_chain.width(), de_swap_chain.height());
+  pipelineConfig.renderPass = de_swap_chain.getRenderPass();
+  pipelineConfig.pipelineLayout = pipeline_layout;
+  de_pipeline = std::make_unique<DePipeline>(
+      de_device,
+      "../shaders/build/vert.spv",
+      "../shaders/build/frag.spv",
+      pipelineConfig);
+}
+
+void Engine::CreatePipelineLayout() {
+  VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+  pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+  pipelineLayoutInfo.setLayoutCount = 0;
+  pipelineLayoutInfo.pSetLayouts = nullptr;
+  pipelineLayoutInfo.pushConstantRangeCount = 0;
+  pipelineLayoutInfo.pPushConstantRanges = nullptr;
+  if (vkCreatePipelineLayout(de_device.device(), &pipelineLayoutInfo, nullptr, &pipeline_layout) !=
+      VK_SUCCESS) {
+    throw runtime_error("failed to create pipeline layout!");
+  }
+}
+
+void Engine::CreateCommandBuffers() {
+
+}
+
+void Engine::DrawFrame() {
+
+}
+
 }
