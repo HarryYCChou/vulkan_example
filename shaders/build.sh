b05a8e413cbd0615ab8d7c0a7ba365587486f27f
Harry Chou <harryyc.chou@gmail.com>
2024-02-29 23:07:35 -0800

    add shader_2 for vertex buffer feature

--- shaders/build.sh
+++ shaders/build.sh
@@ -4,3 +4,5 @@ mkdir -p build
 
 glslc ./src/shader.vert -o build/vert.spv
 glslc ./src/shader.frag -o build/frag.spv
+glslc ./src/shader_2.vert -o build/vert_2.spv
+glslc ./src/shader_2.frag -o build/frag_2.spv
